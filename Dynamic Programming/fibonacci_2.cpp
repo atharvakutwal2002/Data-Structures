7169c1f07f98ef2ec4b4d1c368f19c448f9231fd
Atharva <atharvakutwal2002@gmail.com>
2022-02-14 22:42:52 +0530

    fib 3 added

---
+++ Dynamic Programming/fibonacci_2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+    if (n==1 || n==0)
+    {
+        return 1;
+    }
+    return fib(n-1)+fib(n-2);
+    
+    
+}
+int main(){
+
+    return 0;
+}
