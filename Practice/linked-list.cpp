188acbe3bd2052cf1c02c3b9c812dc23bc1d1e63
Atharva <atharvakutwal2002@gmail.com>
2022-06-07 09:04:21 +0530

    loop detection basics

--- Practice/linked-list.cpp
+++ Practice/linked-list.cpp
@@ -204,6 +204,26 @@ Node *reverese_in_k(Node *&head, int k)
     return prev;
 }
 
+
+bool detect_loop(Node * head){
+    Node * temp= head;
+    if (temp==NULL)
+    {
+        return false;
+    }
+
+    map<Node* , bool> visited;
+    while(temp!= NULL){
+        if (visited[temp]==true)
+        {
+            return true ;
+        }
+        visited[temp]=true;
+        temp= temp->next;
+    }
+    return false;
+}
+
 void reverseLinkedList(Node *&head)
 {
     Node *prev = NULL;
