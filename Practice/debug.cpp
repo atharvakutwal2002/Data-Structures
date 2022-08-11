efba0e71719baaa64b87cca172a99176e77ac265
Atharva <atharvakutwal2002@gmail.com>
2022-08-11 19:26:54 +0530

    power using recursion

---
+++ Practice/debug.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+    if (n > 0)
+    {
+        cout << n << endl;
+    }
+}
+int main()
+{
+    int i = 4;
+    while (i > 0)
+    {
+        print(i);
+        i--;
+    }
+
+    return 0;
+}
--- Practice/tempCodeRunnerFile.cpp
+++ Practice/tempCodeRunnerFile.cpp
@@ -1,8 +1,1 @@
-cout << countNodes(n1) << endl;
-    // isSorted(n1);
-    // search(n1, 70);
-    // display(n1);
-    // deleteNode(1, n1);
-    // display(n1);
-    // sort(n1);
-    // display(n1);
+    int x = 9;
