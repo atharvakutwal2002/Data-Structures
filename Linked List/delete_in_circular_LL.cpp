0a54c1af709797988a9fc9dd6b3eb659c3873272
Atharva <atharvakutwal2002@gmail.com>
2022-08-21 18:53:02 +0530

    delete in a circular LL

---
+++ Linked List/delete_in_circular_LL.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int length(struct Node *p)
+{
+    int len = 0;
+    do
+    {
+        len++;
+        p = p->next;
+    } while (p != head);
+    return len;
+}
+
+int Delete(struct Node *p, int index)
+{
+    struct Node *q;
+    int i, x;
+
+    if (index < 0 || index > Length(Head))
+        return -1;
+    if (index == 1)
+    {
+        while (p->next != Head)
+            p = p->next;
+        x = Head->data;
+        if (Head == p)
+        {
+            free(Head);
+            Head = NULL;
+        }
+        else
+        {
+            p->next = Head->next;
+            free(Head);
+            Head = p->next;
+        }
+    }
+    else
+    {
+        for (i = 0; i < index - 2; i++)
+            p = p->next;
+        q = p->next;
+        p->next = q->next;
+        x = q->data;
+        free(q);
+    }
+    return x;
+}
