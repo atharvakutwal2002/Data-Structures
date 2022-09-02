413bdcc5ca42f4014cc9e88646f8427f815a8390
Atharva <atharvakutwal2002@gmail.com>
2022-09-02 10:45:59 +0530

    next smaller to left

---
+++ Practice/nextSmallerToLeft.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> nsl(vector<int> arr)
+{
+    stack<int> s;
+    vector<int> ans;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (s.empty())
+        {
+            ans.push_back(-1);
+        }
+        else if (s.size() > 0 && s.top() < arr[i])
+        {
+            ans.push_back(s.top());
+        }
+        else if (s.size() > 0 && s.top() >= arr[i])
+        {
+            while (s.size() > 0 && s.top() >= arr[i])
+            {
+                s.pop();
+            }
+            if (s.empty())
+            {
+                ans.push_back(-1);
+            }
+            else
+            {
+                ans.push_back(s.top());
+            }
+        }
+        s.push(arr[i]);
+    }
+    return ans;
+}
