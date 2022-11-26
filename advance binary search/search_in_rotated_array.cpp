b50c0502dc1ad1fb629b4e0b6c265e044fff8cc4
Atharva <atharvakutwal2002@gmail.com>
2022-11-26 17:45:27 +0530

    bubble sort added

--- advance binary search/search_in_rotated_array.cpp
+++ advance binary search/search_in_rotated_array.cpp
@@ -1,12 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 ///  linear search is also accepted  o(n)
-class Solution {
+class Solution
+{
 public:
-    int search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            if( nums[i]==target){
+    int search(vector<int> &nums, int target)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == target)
+            {
                 return i;
             }
         }
@@ -15,31 +19,43 @@ public:
 };
 
 /// optimised binary search approach o(logn)  space comp is 0(1)
-class Solution {
+class Solution
+{
 public:
-    int search(vector<int>& a, int key) {
+    int search(vector<int> &a, int key)
+    {
         int s = 0;
         int e = a.size() - 1;
-        while(s<=e){
-            int mid = (s+e)/2;
-            if(a[mid] == key){
+        while (s <= e)
+        {
+            int mid = (s + e) / 2;
+            if (a[mid] == key)
+            {
                 return mid;
             }
-            
-            if(a[s]<=a[mid]){
-                if(key>=a[s] && key<=a[mid]){
+
+            if (a[s] <= a[mid])
+            {
+                if (key >= a[s] && key <= a[mid])
+                {
                     e = mid - 1;
-                }else{
-                    s = mid + 1;
                 }
-            } else {
-                if(key>=a[mid] && key<=a[e]){
+                else
+                {
                     s = mid + 1;
-                }else{
-                    e = mid - 1; 
                 }
             }
-            
+            else
+            {
+                if (key >= a[mid] && key <= a[e])
+                {
+                    s = mid + 1;
+                }
+                else
+                {
+                    e = mid - 1;
+                }
+            }
         }
         return -1;
     }
